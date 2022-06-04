4fe15f684acc9e1e8036f3a4e9b8cb1a8b021a4d
Muhammad Nur Fadli <muhammadnurf10@gmail.com>
2022-06-04 11:02:38 +0700

    Navbar logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,16 @@ const Navbar = () => {
   return ( 
   <nav className="navbar navbar-expand-lg mb-5 p-3 navbar-dark bg-dark">
   <Link to='/' style={{textDecoration: 'none'}}>
-    <a className="navbar-brand mx-5"> Graph Coloring Simulation </a>
+    <img src='./logo192.png' style={{width:'10%'}} alt='header-logo'/>
+    <a className="navbar-brand mx-4"> Graph Coloring Simulation </a>
   </Link>
   <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div className="navbar-nav">
       <Link to='/detail-laporan-pkl' style={{textDecoration: 'none'}}>
         <a className="nav-item nav-link" > Example </a>
       </Link>
-      <a className="nav-item nav-link " href="https://github.com/wly0601/graph-coloring-simulation" style={{textDecoration: 'none'}}> GitHub Repository </a>
+      <a className="nav-item nav-link" href="https://github.com/wly0601/graph-coloring-simulation" style={{textDecoration: 'none'}}> GitHub Repository </a>
+      <a className="nav-item nav-link" href="https://gitlab.com/wly0601/coloring-graph-simulation" style={{textDecoration: 'none'}}> GitLab Repository </a>
     </div>
   </div>
   </nav>
